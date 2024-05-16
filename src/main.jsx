58cf8df493650dea22e186853dfe5d846306d9ec
MichaelLELU <lelu.michael@gmail.com>
2024-05-16 17:37:40 +0200

    fix branch

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import DetailPage from "./pages/DetailPage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [{
+      path: "/",
+      element: <HomePage/>,
+      loader: () => fetch("https://trefle.io/api/v1/plants?token=tz25VA3y6PKGuKIrAqjsDWEy0DzT-fq9ORXOIU3rPeI")
+    },
+    {
+      path: "/detail/:id",
+      element:<DetailPage/>
+    },
+    {
+      path: "/about",
+      element:<AboutPage/>
+    },
+    {
+      path: "*",
+      element:<h1>Page not found Error 404 </h1>
+    }],
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
