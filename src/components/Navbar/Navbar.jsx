b5af2e08ece82f3800fa2dee6f0177bb79a3326d
angelique888 <angeliquegrall5@gmail.com>
2024-05-17 08:40:56 +0200

    feat: redirection Homepage logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,9 +4,17 @@ import "./Navbar.css";
 import logo from "../../assets/logo.png";
 
 export default function Navbar() {
+
+  const redirectionToHome = () => {
+    window.location.href = "/";
+  };
   return (
     <nav className="Menu">
+      <div>
+      <button type="button" onClick={redirectionToHome}>
       <img className="logo" src={logo} alt="logo BotanicWorld" />
+      </button>
+      </div>
       <h1 className="Titre">Botanic World</h1>
       <div className="desktop">
         <Link id="button_home" to="/">
