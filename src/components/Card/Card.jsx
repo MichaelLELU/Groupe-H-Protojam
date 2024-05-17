f0044d2944fe485f8ff1502669ce52cd28f9e4bf
MichaelLELU <lelu.michael@gmail.com>
2024-05-17 12:44:07 +0200

    fix: affichages

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,7 +8,7 @@ export default function Card ({item}) {
     return (
         <div className="card">
             <h3>{item.common_name}</h3>
-            <Link to={`detail/${item.id}`} ><img src={item.image_url} alt={item.common_name} /></Link>   
-    </div>
+            <Link to={`detail/${item.id}`} ><img src={item.image_url} alt={item.common_name} /></Link> 
+    </div> 
     )
 }
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -5,11 +5,12 @@ export default function CardList({result}){
     return(
         <div className="card-container">
         {result.data.map((item,i) => (
+           item.image_url ?
            <Card
            className="card"
            key = {i}
             item={item}
-           />
+           /> : null
         )
         )}
         </div>
